5554c2367d2869b79e5aea5c87de505b5fe1ae88
apaardahal <cullaapar@gmail.com>
2021-08-23 22:24:13 +0545

    Topbar completed

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -1,15 +1,27 @@
 import React from 'react'
 import './topbar.css'
+import { NotificationsNone, Language, Settings} from '@material-ui/icons'
 
 export default function Topbar() {
     return (
         <div className = "topbar">
             <div className = "topbarWrapper">
                 <div className = "topLeft">
-                    Left
+                   <span className = "logo">ApaarAdmin</span>
                 </div>  
                 <div className = "topRight">
-                    Right
+                    <div className = "topbarIconContainer">
+                            <NotificationsNone/>
+                            <span className = "topIconBadge">2</span>
+                    </div>
+                    <div className = "topbarIconContainer">
+                            <Language/>
+                            <span className = "topIconBadge">2</span>
+                    </div>
+                    <div className = "topbarIconContainer">
+                            <Settings/>
+                    </div>
+                    <img src = "/assets/apaar.jpg" className = "topAvatar"/>
                 </div>
             </div>
         </div>
