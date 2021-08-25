b21d38264fbae38bb2e8d043db15deb2fed779be
apaardahal <cullaapar@gmail.com>
2021-08-26 00:25:45 +0545

    Chart implemented

---
+++ src/dummyData.js
@@ -0,0 +1,50 @@
+export  const userData = [
+    {
+      name: 'Jan',
+      "Active User": 4000,
+    },
+    {
+      name: 'Feb',
+      "Active User": 6000,
+    },
+    {
+      name: 'Mar',
+      "Active User": 2000,
+    },
+    {
+      name: 'Apr',
+      "Active User": 2780,
+    },
+    {
+      name: 'May',
+      "Active User": 5000,
+    },
+    {
+      name: 'Jun',
+      "Active User": 2000,
+    },
+    {
+      name: 'July',
+      "Active User": 3490,
+    },
+      {
+        name: 'Aug',
+        "Active User": 6000,
+      },
+      {
+        name: 'Sep',
+        "Active User": 3490,
+      },
+      {
+        name: 'Oct',
+        "Active User": 3560,
+      },
+      {
+        name: 'Nov',
+        "Active User": 2560,
+      },
+      {
+        name: 'Dec',
+        "Active User": 6000,
+      },
+  ];
