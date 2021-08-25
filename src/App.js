92e4866e3700f02805f881e2259d296cc1279459
apaardahal <cullaapar@gmail.com>
2021-08-25 23:46:14 +0545

    Featured information

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 
 import Sidebar from './components/sidebar/Sidebar';
 import Topbar from './components/topbar/Topbar';
+import Home from './pages/home/Home';
 import './app.css';
 
 function App() {
@@ -9,7 +10,7 @@ function App() {
           <Topbar/>
           <div className = "container">
               <Sidebar/>
-             <div  className = "others">other pages</div> 
+             <Home/>
           </div>
     </div>
   );
