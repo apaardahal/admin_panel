c061ee17ca5924339b86e1666ce52cfedb921358
apaardahal <cullaapar@gmail.com>
2021-09-04 09:52:52 +0545

    Userlist component

--- src/pages/userList/UserList.jsx
+++ src/pages/userList/UserList.jsx
@@ -1,53 +1,72 @@
-import './userlist.css';
-import { DataGrid } from '@material-ui/data-grid';
+import "./userlist.css";
+import { DataGrid } from "@material-ui/data-grid";
+import { DeleteOutline } from "@material-ui/icons";
+import { userRows } from "../../dummyData";
+import { Link } from "react-router-dom";
+import { useState } from "react";
 
 export default function UserList() {
+  const [data, setData] = useState(userRows);
 
-    const columns = [
-        { field: 'id', headerName: 'ID', width: 90 },
-        {
-          field: 'username',
-          headerName: 'User name',
-          width: 200,
-          editable: true,
-        },
-        {
-          field: 'gmail',
-          headerName: 'Email',
-          width: 200,
-          editable: true,
-        },
-        {
-          field: 'status',
-          headerName: 'Status',
-          width: 120,
-          editable: true,
-        },
-        {
-          field: 'transaction',
-          headerName: 'Transaction',
-          description: 'This column has a value getter and is not sortable.',
-          sortable: false,
-          width: 160
-        },
-      ];
-      
-      const rows = [
-        { id: 1, username: 'Apaar', avatar: '/assets/apaar.jpg', gmail: 'apaar@gmail.com', status: 'active', transaction: '$500'},
-        { id: 2, username: 'Apaar', avatar: '/assets/apaar.jpg', gmail: 'apaar@gmail.com', status: 'active', transaction: '$500'},
-        { id: 3, username: 'Apaar', avatar: '/assets/apaar.jpg', gmail: 'apaar@gmail.com', status: 'active', transaction: '$500'},
-        { id: 4, username: 'Apaar', avatar: '/assets/apaar.jpg', gmail: 'apaar@gmail.com', status: 'active', transaction: '$500'},
-        { id: 5, username: 'Apaar', avatar: '/assets/apaar.jpg', gmail: 'apaar@gmail.com', status: 'active', transaction: '$500'},
-      ];
-    return (
-        <div className = "userList">
-                <DataGrid
-                        rows={rows}
-                        columns={columns}
-                        pageSize={5}
-                        checkboxSelection
-                        disableSelectionOnClick
-                    />
-        </div>
-    )
+  const handleDelete = (id) => {
+    setData(data.filter((item) => item.id !== id));
+  };
+  
+  const columns = [
+    { field: "id", headerName: "ID", width: 90 },
+    {
+      field: "user",
+      headerName: "User",
+      width: 200,
+      renderCell: (params) => {
+        return (
+          <div className="userListUser">
+            <img className="userListImg" src={params.row.avatar} alt="" />
+            {params.row.username}
+          </div>
+        );
+      },
+    },
+    { field: "email", headerName: "Email", width: 200 },
+    {
+      field: "status",
+      headerName: "Status",
+      width: 120,
+    },
+    {
+      field: "transaction",
+      headerName: "Transaction Volume",
+      width: 160,
+    },
+    {
+      field: "action",
+      headerName: "Action",
+      width: 150,
+      renderCell: (params) => {
+        return (
+          <>
+            <Link to={"/user/" + params.row.id}>
+              <button className="userListEdit">Edit</button>
+            </Link>
+            <DeleteOutline
+              className="userListDelete"
+              onClick={() => handleDelete(params.row.id)}
+            />
+          </>
+        );
+      },
+    },
+  ];
+
+  return (
+    <div className="userList">
+      <DataGrid
+        rows={data}
+        disableSelectionOnClick
+        columns={columns}
+        pageSize={8}
+        checkboxSelection
+      />
+    </div>
+  );
 }
